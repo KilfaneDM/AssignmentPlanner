0fcab9fa14b09be480d94f7db9c97c4d5e82cde7
Tyler Demers <tylerdemers@hotmail.com>
2025-11-06 03:37:28 -0400

    Created Template for login page

---
+++ AssignmentPlanner/AssignmentPlanner/Controllers/AccountController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using AssignmentPlanner.Models;
+namespace AssignmentPlanner.Controllers
+{
+    public class AccountController : Controller
+    {
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(LoginViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                // For now, just show what was entered or redirect somewhere.
+                ViewBag.Message = $"You entered Username: {model.Username}, Password: {model.Password}";
+                // You could redirect instead:
+                // return RedirectToAction("Index", "Home");
+            }
+
+            return View(model);
+        }
+    }
+}
