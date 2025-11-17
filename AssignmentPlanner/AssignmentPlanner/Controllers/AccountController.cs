e7534b1237f743a8d50b4f262a68a04ce6bd4855
Tyler <tylerdemers@hotmail.com>
2025-11-17 18:02:25 -0400

    created template files for working on the dashboards

--- AssignmentPlanner/AssignmentPlanner/Controllers/AccountController.cs
+++ AssignmentPlanner/AssignmentPlanner/Controllers/AccountController.cs
@@ -1,9 +1,18 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using AssignmentPlanner.Models;
+using System.Linq;
+
 namespace AssignmentPlanner.Controllers
 {
     public class AccountController : Controller
     {
+        private readonly AssignmentPlannerDbContext _context;
+
+        public AccountController(AssignmentPlannerDbContext context)
+        {
+            _context = context;
+        }
+
         [HttpGet]
         public ActionResult Login()
         {
@@ -13,15 +22,30 @@ namespace AssignmentPlanner.Controllers
         [HttpPost]
         public ActionResult Login(LoginViewModel model)
         {
-            if (ModelState.IsValid)
+            if (!ModelState.IsValid)
+                return View(model);
+
+            var login = _context.Logins
+                .FirstOrDefault(l => l.Nbccid.ToString() == model.Username && l.Password == model.Password);
+
+            if (login == null)
             {
-                // For now, just show what was entered or redirect somewhere.
-                ViewBag.Message = $"You entered Username: {model.Username}, Password: {model.Password}";
-                // You could redirect instead:
-                // return RedirectToAction("Index", "Home");
+                TempData["LoginError"] = "Username or password is incorrect.";
+                return View(model);
             }
 
-            return View(model);
-        }
+            switch (login.UserType)
+            {
+                case "S":
+                    return RedirectToAction("Index", "StudentDashboard");
+                case "I":
+                    return RedirectToAction("Index", "InstructorDashboard");
+                case "A":
+                    return RedirectToAction("Index", "AdministrationDashboard");
+                default:
+                    TempData["LoginError"] = "Unknown user type.";
+                    return View(model);
+            }
+        }s
     }
 }
